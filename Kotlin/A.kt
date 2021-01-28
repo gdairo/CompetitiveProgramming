938da476871062b55c3deea1e0e8c90e7590d8c4
Dairo <gdairo094@gmail.com>
2021-01-28 02:12:23 -0500

    Comentarios

--- Kotlin/A.kt
+++ Kotlin/A.kt
@@ -1,10 +1,19 @@
-tailrec fun removeZeroes(x: Int): Int = if (x % 10 == 0) removeZeroes(x / 10) else x
+/**
+* @Author Dairo Garcia
+* 
+* https://codeforces.com/contest/1157/problem/A
+*
+*/
 
-fun f(x: Int) = removeZeroes(x + 1)  
+tailrec fun quitarZeros(x: Int): Int = if (x % 10 == 0) quitarZeros(x / 10) else x
+
+fun f(x: Int) = quitarZeros(x + 1)  
 
 fun main() {
     var n = readLine()!!.toInt()
-    var reached = HashSet<Int>()
-    while(reached.add(n)) n = f(n)
-    println(reached.size)
-} 
+
+    var alcanzado = HashSet<Int>()
+    while(alcanzado.add(n)) n = f(n)
+    
+    println(alcanzado.size)
+}
