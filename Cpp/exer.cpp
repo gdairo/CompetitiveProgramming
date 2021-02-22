c7548da54435560465d4c999165a2721384f3f8d
Dairo <gdairo094@gmail.com>
2021-02-22 10:25:26 -0500

    Exersice 2

---
+++ Cpp/exer.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <ios>
+using namespace std;
+
+#define endl '\n';
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+
+    int n;
+
+    cin >> n;
+    cout << 5000 + M_PI << endl;
+    cout << setprecision(n + 1) << M_PI << endl;
+
+    return 0;
+}
